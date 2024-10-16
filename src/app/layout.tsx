d2110f5771a8bc7fd2b8a1a6d1e706db0ace25fc
MaryamJami1 <maryamchouhan373@gmail.com>
2024-10-16 23:42:15 +0500

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import StarsCanvas from "../../components/background";
+import Navbar from "../../components/navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +30,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
+        <StarsCanvas/>
+        <Navbar/>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/header'
+import Skills from '../../components/skills'
 
 
 export default function page() {
   return (
     <main className='h-full w-full'>
-      <div className='flex flex-col h-850px[] gap-20'>
+      <div className='flex flex-col gap-20'>
         <Header />
+        <Skills/>
       </div>
     </main>
   )
