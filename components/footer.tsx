17b27be985a48eb740a19fc62e4d5fb5c396c814
MaryamJami1 <maryamchouhan373@gmail.com>
2024-10-20 17:23:55 +0500

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,17 +11,14 @@ import { FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
+    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[20px] mt-11">
         <div className="w-full flex flex-col items-center justify-center m-auto">
             <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
                 
 
                 <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">Community</div>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <FaYoutube />
-                        <span className="text-[15px] ml-[6px]">Youtube</span>    
-                    </p>
+                    <div className="font-bold text-[16px]">Community</div> 
+                   
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                         <RxGithubLogo />
                         <span className="text-[15px] ml-[6px]">Github</span>    
@@ -37,24 +34,18 @@ const Footer = () => {
                         <FaYoutube />
                         <span className="text-[15px] ml-[6px]">Instagram</span>    
                     </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxGithubLogo />
-                        <span className="text-[15px] ml-[6px]">Twitter</span>    
-                    </p>
+                  
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                         <RxDiscordLogo />
                         <span className="text-[15px] ml-[6px]">Linkedin</span>    
                     </p>
                 </div>
                 <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">About</div>
-                   <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                     
-                        <span className="text-[15px] ml-[6px]">Become Sponsor</span>    
-                    </p>
+                    <div className="font-bold text-[16px]">Contact</div>
+                  
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                      
-                        <span className="text-[15px] ml-[6px]">Learning about me</span>    
+                        <RxDiscordLogo />
+                        <span className="text-[15px] ml-[6px]">Linkedin</span>    
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                   
@@ -63,7 +54,7 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className="mb-[20px] text-[15px] text-center">
+            <div className="my-[20px] text-[15px] text-center">
                 &copy; Maryam jamil Inc. All rights reserved
             </div>
         </div>
