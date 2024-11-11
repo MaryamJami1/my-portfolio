937a72dba1f4492cdf263c2cbb58940b0b088785
Maryam Jamil <154235773+MaryamJami1@users.noreply.github.com>
2024-11-11 22:31:53 +0500

    Update project.tsx

--- components/project.tsx
+++ components/project.tsx
@@ -7,7 +7,7 @@ const Projects = () => {
     <div
       className="flex flex-col items-center justify-center pt-7 pb-[5rem]" id="projects">
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-16">
-        My Projects
+        My Recent Projects
       </h1>
       <div className="h-full w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-10 ">
         <a
